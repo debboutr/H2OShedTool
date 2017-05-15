d21fde965a29ff69f9c29ed5b1d8506dd6653f36
rickD <debboutr@gmail.com>
2017-05-15 08:23:44 -0700

    functionalize main

--- H2OShed.py
+++ H2OShed.py
@@ -4,123 +4,147 @@ Created on Tue Feb 21 12:55:48 2017
 
 @author: Rdebbout
 """
+
+import os
 import numpy as np
-import rasterio as rs
 import geopandas as gpd
 import georasters as gr
 
+DIRS = {0:16,1:32,2:64,3:128,4:1,5:2,6:4,7:8}
 
-
-def makeDirections(pp):    
-    zero = (pp[0],pp[1]+1)
-    one = (pp[0]+1,pp[1]+1)
-    two = (pp[0]+1,pp[1])
-    three = (pp[0]+1,pp[1]-1)
-    four = (pp[0],pp[1]-1)
-    five = (pp[0]-1,pp[1]-1)
-    six = (pp[0]-1,pp[1])
-    seven = (pp[0]-1,pp[1]+1)
+def ringCells(p):
+    '''
+    '''
+    zero = (p[0],p[1]+1)
+    one = (p[0]+1,p[1]+1)
+    two = (p[0]+1,p[1])
+    three = (p[0]+1,p[1]-1)
+    four = (p[0],p[1]-1)
+    five = (p[0]-1,p[1]-1)
+    six = (p[0]-1,p[1])
+    seven = (p[0]-1,p[1]+1)
     return [zero,one,two,three,four,five,six,seven]
 
-def circleCell(dirs):
-    for i in range(len(dirs)):
-        if i == 0 and arr.raster[dirs[i]] == 16:
-            hold.append(dirs[i])
-        if i == 1 and arr.raster[dirs[i]] == 32:
-            hold.append(dirs[i])
-        if i == 2 and arr.raster[dirs[i]] == 64:
-            hold.append(dirs[i])
-        if i == 3 and arr.raster[dirs[i]] == 128:
-            hold.append(dirs[i])
-        if i == 4 and arr.raster[dirs[i]] == 1:
-            hold.append(dirs[i])
-        if i == 5 and arr.raster[dirs[i]] == 2:
-            hold.append(dirs[i])
-        if i == 6 and arr.raster[dirs[i]] == 4:
-            hold.append(dirs[i])
-        if i == 7 and arr.raster[dirs[i]] == 8:
-            hold.append(dirs[i])
-    return hold    
+def evalRing(ring,ras):
+    '''
+    '''
+    for c in range(len(ring)):
+        if c == 0 and ras.raster[ring[c]] == 16:
+            hold.append(ring[c])
+        if c == 1 and ras.raster[ring[c]] == 32:
+            hold.append(ring[c])
+        if c == 2 and ras.raster[ring[c]] == 64:
+            hold.append(ring[c])
+        if c == 3 and ras.raster[ring[c]] == 128:
+            hold.append(ring[c])
+        if c == 4 and ras.raster[ring[c]] == 1:
+            hold.append(ring[c])
+        if c == 5 and ras.raster[ring[c]] == 2:
+            hold.append(ring[c])
+        if c == 6 and ras.raster[ring[c]] == 4:
+            hold.append(ring[c])
+        if c == 7 and ras.raster[ring[c]] == 8:
+            hold.append(ring[c])
+    return hold   
+ 
+    
+def evalRing(ring,ras):
+    '''
+    '''
+    for c in DIRS:
+        if ras.raster[ring[c]] == DIRS[c]:
+            hold.append(ring[c])
+    return hold 
+
+
+def Watershed(fdrf,ptf,val=47):
+    ''' 2-D Array (row,col)
+    '''  
+    NDV, xsize, ysize, tf, Projection, DataType = gr.get_geo_info(fdrf)
+    arr = gr.from_file(fdrf)
+    (xmin, xsize, x, ymax, y, ysize) = arr.geot
+    pt = gpd.read_file(ptf)
+    lon,lat = list(pt.geometry[0].coords)[0]
+    here = gr.map_pixel(lon, lat,xsize,ysize,xmin,ymax)    
+    init = ringCells(here)
+    hold = []
+    hold = evalRing(init,arr)
+    for x in hold:
+        init = ringCells(x)
+        hold = evalRing(init,arr)     
+    hold.append(here)
+    
+    # try to update tf and shrink array
+    dd = np.array(hold)
+    r_min, col_min = np.amin(dd,axis=0)
+    r_max, col_max = np.amax(dd,axis=0)
+#    # below is for creating array after tranform adjust
+#    # may be too intensive to justify
+#    (r_max - r_min, col_max - col_min)
+
+    # shift transform to reduce NoData in the output raster
+    shifted_tf = (tf[0] + r_min * tf[-1],tf[1],tf[2],tf[3] - col_min * tf[1], tf[4],tf[5])
+    # insert val into the cells of the raster
+    out = np.zeros(arr.shape)
+    for idx in hold:
+        out[idx] = val
         
-#    yes.append(hold)
-#    if len(hold) > 0:
-#        for x in hold:
-#            dirs = makeDirections(x)
-#            circleCell(dirs)
-#    else:
-#        return yes
+    new = out[r_min:r_max,col_min:col_max]
+    
+    go = gr.GeoRaster(new,shifted_tf, 0)
+    go.projection = Projection
+    go.datatype = DataType
+    
+    fn = os.path.basename(ptf).split('.')[0]   
+    loc = os.path.dirname(ptf)
+    go.to_tiff('{}/{}_watershed_ed'.format(loc,fn))
 
-   
-# find the pour point cell with georasters
-raster = 'D:/Projects/H2OShed_Tool/wsALSS-1065/fdr'
-NDV, xsize, ysize, GeoT, Projection, DataType = gr.get_geo_info(raster)
-arr = gr.from_file(raster)
+if __name__ == '__main__':
+    flow = '/home/rick/projects/splitCats/fdr.tif'
+    ptfn = '/home/rick/projects/splitCats/point_5070.shp'
+    Watershed(flow,ptfn,55)
+    
+    
+    
 
-(xmin, xsize, x, ymax, y, ysize) = arr.geot
-
-pt = gpd.read_file('D:/Projects/H2OShed_Tool/wsALSS-1065/PointShape.shp')
-lon,lat = list(pt.geometry[0].coords)[0]
-
-here = gr.map_pixel(lon, lat,xsize,ysize,xmin,ymax)
-#arr.raster[here]
-
-init = makeDirections(here)
-
-
-hold = []
-
-
-hold = circleCell(init)
-for x in hold:
-    init = makeDirections(x)
-    circleCell(init)
-        
-hold.append(here)
-out = np.zeros(arr.shape)
-val = 47
-for idx in hold:
-    out[idx] = val
-go = gr.GeoRaster(out,arr.geot, 0)
-go.projection = Projection
-go.datatype = DataType
-
-go.to_tiff('D:/Projects/H2OShed_Tool/wsALSS-1065/yeah')
+#gdalwarp -q -cutline /home/rick/projects/splitCats/cat_5070.shp 
+#-tr 30.0 30.0 -of GTiff -co COMPRESS=LZW  
+#/media/rick/gitSum/NHDPlusv21/NHDPlusPN/NHDPlus17/NHDPlusFdrFac17c/fdr
+#/home/rick/projects/splitCats/fdr2.tif  
 ##############################################################################
-kk = []
-for x, y in zip((range(0,10)),(range(10,20))):
-    kk.append((x,y))
-    
-mm = []
-for x, y in zip((range(0,10)),(range(20,30))):
-    mm.append((x,y))
-    
-yy = mm[:3]
-yy[0] = (0,20)
+#kk = []
 
-import numpy as np  # this is where you use pandas!
-
-rr = np.array(mm)
-dd = np.array(yy)
+#for x, y in zip((range(0,10)),(range(10,20))):
+#    kk.append((x,y))
+#    
+#mm = []
+#for x, y in zip((range(0,10)),(range(20,30))):
+#    mm.append((x,y))
+#    
+#yy = mm[:3]
+#yy[0] = (0,20)
+#
+#import numpy as np  # this is where you use pandas!
+#
+#rr = np.array(mm)
+#dd = np.array(yy)
 
 # sort tuples on x val
 
 # try solution from the NHD Lakes QA script
 
-dd in rr
-
-import pandas as pd
-
-ww = pd.DataFrame(dd, columns=['lon','lat'], index=[47 for i in range(len(dd))])
-aa = pd.DataFrame(rr, columns=['lon','lat'])
+#dd in rr
+#
+#import pandas as pd
+#
+#ww = pd.DataFrame(dd, columns=['lon','lat'], index=[47 for i in range(len(dd))])
+#aa = pd.DataFrame(rr, columns=['lon','lat'])
 
 # stack
 
 # copy
 
 # drop_duplicates -- iterate here
-
-range?
-pd.DataFrame?
 
 
 # would it always be he smallest one that gets the val? ..yes!
@@ -129,7 +153,7 @@ pd.DataFrame?
 # one table w/ dups dropped to iterate
 
 # hold dict of key=COMID, val=length of retained array thru 
-# circleCell process to select the one to keep
+# evalRing process to select the one to keep
 
 # find all of the indexes in the DF where x/y exist,
 # append/hold that index in new DF/drop from existing
@@ -165,7 +189,7 @@ pd.DataFrame?
 #pp = (3,3)
 #arr[pp]
 
-#ds = makeDirections(pp)
+#ds = ringCells(pp)
 #for p in ds:
 #    print arr[p]
 
